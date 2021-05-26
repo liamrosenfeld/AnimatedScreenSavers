d640cfd3a5c10d8624f4a309a50a8ce98543d4e2
Liam Rosenfeld <me@liamrosenfeld.com>
2021-05-26 15:45:46 -0400

    Initial Commit

---
+++ AnimatedScreenSavers/AnimatedScreenSaversView.h
@@ -0,0 +1,12 @@
+//
+//  AnimatedScreenSaversView.h
+//  AnimatedScreenSavers
+//
+//  Created by Liam Rosenfeld on 5/26/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface AnimatedScreenSaversView : ScreenSaverView
+
+@end
---
+++ AnimatedScreenSavers/AnimatedScreenSaversView.m
@@ -0,0 +1,51 @@
+//
+//  AnimatedScreenSaversView.m
+//  AnimatedScreenSavers
+//
+//  Created by Liam Rosenfeld on 5/26/21.
+//
+
+#import "AnimatedScreenSaversView.h"
+
+@implementation AnimatedScreenSaversView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
